2a6fffd932b5b8d80157147b1a74c86893f80005
Christine <74520672+chrish8510@users.noreply.github.com>
2021-06-20 18:17:42 -0400

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,8 @@
+# user input for the year
 leapYear = input('Enter Current Year: ')
+# name the variable input to year_num
 year_num = int(leapYear)
-
+# leap year happens once every 4 years. if function is for the leap year, and else function is for other than leap year.
 if year_num % 4 == 0:
     print('Year', year_num,  'is a leap year.')
 else:
